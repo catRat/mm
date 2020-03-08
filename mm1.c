526a7a03ceb16e03403eb363a4dc1d61ad0c6ecb
周明东 <505732590@qq.com>
2020-03-08 20:37:32 +0800

    allhead -> stdio

--- mm1.c
+++ mm1.c
@@ -1,12 +1,10 @@
-@ Here xxx
-@c
 int main(void)
 {
 	/**123*/
 	string const SS =
 		"P=j\n"
 		"OBJECTS=\n"
-		"CFLAGS = -g -Wall -O2 -std=gnu11 -include allhead.h \n"
+		"CFLAGS = -g -Wall -O2 -std=gnu11 -include stdio.h \n"
 		"LDLIBS=\n"
 		"cc=gcc\n"
 		"${P}:";
